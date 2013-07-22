1eb6410bbcdc3a4a9da301408b37bfd19cb50c99
felixzhao <felixz2010@gmail.com>
2013-07-22 16:22:30 +0800

    This Project is for export mysql datatable to Excel, and import Excel file to mysql datatable'

---
+++ Export2ExcelFromMysql/DtExport2Excel.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DtExport2Excel.aspx.cs" Inherits="ExportToExcelFromDataTable.Practice" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        <asp:Button runat="server" ID="Btn_Export" Text="Export" OnClick="Btn_Export_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Export2ExcelFromMysql/MySqlHelper.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using MySql.Data.MySqlClient;
+
+namespace BIG
+{
+    public class MySqlHelper
+    {
+        private readonly MySqlConnection _connection;
+
+        private readonly MySqlCommand _command;
+
+        private readonly string _connectionString;
+
+        public MySqlHelper(string connectionString)
+        {
+           // _connectionString = System.Configuration.ConfigurationManager.ConnectionStrings[connectionStringKey].ToString();
+            _connectionString = connectionString;
+            _connection = new MySqlConnection(_connectionString);
+            _command = new MySqlCommand("", _connection);
+        }
+
+        public MySqlDataReader ExecuteReader(string cmdText)
+        {
+            try
+            {
+                _connection.Open();
+                _command.CommandText = cmdText;
+                return _command.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+            catch (MySqlException)
+            {
+                if (_connection.State != ConnectionState.Closed)
+                {
+                    _connection.Close();
+                }
+                return null;
+            }
+        }
+
+        public object ExecuteScalar(string cmdText)
+        {
+            try
+            {
+                _connection.Open();
+                _command.CommandText = cmdText;
+                return _command.ExecuteScalar();
+            }
+            catch (MySqlException)
+            {
+                return null;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+        public bool ExecuteNonQuery(string cmdText, List<MySqlParameter> parameters)
+        {
+            try
+            {
+                _connection.Open();
+                _command.CommandText = cmdText;
+                _command.CommandType = CommandType.StoredProcedure;
+                _command.Parameters.Add(parameters);
+                return _command.ExecuteNonQuery() > 0;
+            }
+            catch (MySqlException)
+            {
+                return false;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+        public bool ExecuteNonQuery(string cmdText)
+        {
+            try
+            {
+                _connection.Open();
+                _command.CommandText = cmdText;
+                return _command.ExecuteNonQuery() > 0;
+            }
+            catch (MySqlException)
+            {
+                return false;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+        public void Statistic(string url)
+        {
+            var count = Convert.ToInt32(ExecuteScalar("select count(*) from big_statistics_click where vdate='" + DateTime.Now.Year + "-" + DateTime.Now.Month + "-" + DateTime.Now.Day + "' and pn='" + url + "'"));
+            var str = count > 0
+                          ? "update big_statistics_click set pv=pv+1  where  vdate='" + DateTime.Now.Year + "-" + DateTime.Now.Month + "-" + DateTime.Now.Day +
+                            "' and pn='" + url + "'"
+                          : "insert into big_statistics_click(vdate,pn,pv) values('" + DateTime.Now.Year + "-" + DateTime.Now.Month + "-" + DateTime.Now.Day + "','" + url + "',1)";
+            ExecuteNonQuery(str);
+        }
+
+        public void StatisticLogin(string loginname,string name,string role,string department)
+        {
+            var sql = "insert into big_loginstatistics(loginname,name,role,department,logintime) values('"+loginname+"','" + name + "','" + role + "','" + department + "','" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "')";
+            ExecuteNonQuery(sql);
+        }
+
+        public DataSet ExecuteDataSet(string sql)
+        {
+            try
+            {
+                _connection.Open();
+                var myDa = new MySqlDataAdapter(sql, _connection);
+                var myds = new DataSet();
+                myDa.Fill(myds);
+                return myds;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+        public string StatisticDengji(string loginname)
+        {
+            var reader = ExecuteReader("select logintime from big_loginstatistics a where a.loginname='" + loginname + "'");
+            List<string> list = null;
+            if (reader != null)
+            {
+                list = new List<string>();
+                while (reader.Read())
+                {
+                    list.Add(Convert.ToDateTime(reader["logintime"]).ToString("yyyy-MM-dd"));
+                }
+                reader.Close();
+            }
+            if (list != null)
+                return list.Distinct().Count() + "";
+            return "0";
+        }
+
+        public void Close()
+        {
+            if (_connection.State != ConnectionState.Closed)
+            {
+                _connection.Close();
+            }
+        }
+    }
+}
